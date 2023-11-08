d7368b97bef409eef76ff4fa9f2d7f7f50cdf220
Mr-BD <borislav.draev7@gmail.com>
2023-11-08 13:07:25 +0200

    Eddits

--- src/SwitchCase.java
+++ src/SwitchCase.java
@@ -14,7 +14,19 @@ public class SwitchCase {
             System.out.println("Tuesday");
             break;
         case 3:
-            System.out.println("Wendsday");
+            System.out.println("Wednesday");
+            break;
+        case 4:
+            System.out.println("Thursday");
+            break;
+        case 5:
+            System.out.println("Friday");
+            break;
+        case 6:
+            System.out.println("Saturday");
+            break;
+        case 7:
+            System.out.println("Sunday");
             break;
         default:
             System.out.println("this is the end!");
